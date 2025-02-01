cd525035005c4c4fb0d1e3ab3b7044b6571ceb6e
parisfuchsia <liammagat8@gmail.com>
2025-02-01 08:03:29 +0800

    argh

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: process.env.CORS_ORIGIN || "https://notes-from-a-stranger-pls.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
